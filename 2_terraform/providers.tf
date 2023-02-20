c3d9cc6ce1ea89f10f820b02b8a339755abe0d6e
Shved.A.V <shvedav86@gmail.com>
2023-02-20 22:01:17 +0200

    change2 providers.tf

--- 2_terraform/providers.tf
+++ 2_terraform/providers.tf
@@ -14,9 +14,9 @@ terraform {
 }
 
 provider "azurerm" {
-  client_id       = ${env:ARM_CLIENT_ID}
-  client_secret   = ${env:ARM_CLIENT_SECRET}
-  tenant_id       = ${env:ARM_TENANT_ID}
-  subscription_id = ${env:ARM_SUBSCRIPTION_ID}
+  client_id       = ${ARM_CLIENT_ID}
+  client_secret   = ${ARM_CLIENT_SECRET}
+  tenant_id       = ${ARM_TENANT_ID}
+  subscription_id = ${ARM_SUBSCRIPTION_ID}
   features {}
 }
