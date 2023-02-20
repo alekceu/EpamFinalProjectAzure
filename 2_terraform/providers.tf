c04fda012b341a7f156365393236a00dc9a77fbe
Shved.A.V <shvedav86@gmail.com>
2023-02-20 20:43:19 +0200

    add az cred to providers.tf

--- 2_terraform/providers.tf
+++ 2_terraform/providers.tf
@@ -14,5 +14,9 @@ terraform {
 }
 
 provider "azurerm" {
+  client_id       = "0bf2a8cd-f2b2-4be2-8ffc-36772de5a639"
+  client_secret   = "gTi8Q~3i7RH--fJ3fgZNmI1ZAAck5YsG8d4SactZ"
+  tenant_id       = "de82e085-b114-496d-b42f-221b3c9f84f2"
+  subscription_id = "4ddbf480-32c8-4bd9-99fb-7cea419d8be2"
   features {}
 }
