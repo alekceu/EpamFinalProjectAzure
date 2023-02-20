00a60bdf46b9778480a1077162207374edf5fe4b
Shved.A.V <shvedav86@gmail.com>
2023-02-20 21:32:42 +0200

    change providers.tf

--- 2_terraform/providers.tf
+++ 2_terraform/providers.tf
@@ -1,3 +1,8 @@
+variable "var_client_id" {}
+variable "var_client_secret" {}
+variable "var_tenant_id" {}
+variable "var_subsc_id" {}
+
 terraform {
   required_version = ">=0.12"
 
@@ -14,5 +19,9 @@ terraform {
 }
 
 provider "azurerm" {
+  client_id       = var.var_client_id
+  client_secret   = var.var_client_secret
+  tenant_id       = var.var_tenant_id
+  subscription_id = var.var_subsc_id
   features {}
 }
