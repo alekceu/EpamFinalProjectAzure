a3a4f9047d49f81f0f20231c9a9ae78cf36c0422
Shved.A.V <shvedav86@gmail.com>
2023-02-21 23:58:15 +0200

    Change IP allocation

--- 2_terraform/main.tf
+++ 2_terraform/main.tf
@@ -55,7 +55,7 @@ resource "azurerm_network_interface" "AZVM_nic" {
   ip_configuration {
     name                          = "AZVM_nic_configuration"
     subnet_id                     = azurerm_subnet.az_subnet.id
-    private_ip_address_allocation = "Static"
+    private_ip_address_allocation = "Dynamic"
     public_ip_address_id          = azurerm_public_ip.az_public_ip.id
   }
 }
